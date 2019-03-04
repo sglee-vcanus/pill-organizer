f1bfef37d6424cde77c5774b3da72f69103e174c
sglee <sg10.lee@gmail.com>
2019-03-04 19:21:43 +0900

    Initial Commit

---
+++ pill-organizer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  pill-organizer
+//
+//  Created by sglee on 04/03/2019.
+//  Copyright © 2019 sglee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
